d28b83bfc9fba19497407aa70f4111a2340db8ac
Muhammad Usman Khan Joiya <156664705+Usmankhanjoiya@users.noreply.github.com>
2024-03-30 04:08:35 +0500

    Add files via upload

---
+++ green.protocol/js/green.js
@@ -0,0 +1,11 @@
+
+
+// const section_Problem = document.querySelector(".section_Problem");
+
+
+// const observer = new IntersectionObserver( = {}, {
+//     root: null,
+//     rootMargin: "",
+//     threshold: "0",
+// });
+// observer.observe(section_Problem);
